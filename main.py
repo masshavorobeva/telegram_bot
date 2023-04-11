3469e7b8955bb81df1f12e39a0b8a27fe9a422f2
Yaroslav Samokhin <samokhin.yaroslav@gmail.com>
2023-04-11 22:21:29 +0300

    добавление запроса контакта

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ def start(message):
 
 @bot.message_handler(content_types=['contact'])
 def contact(message):
+    if message.contact is not None:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+        btn1 = types.KeyboardButton('1')
+        btn2 = types.KeyboardButton('2')
+        markup.add(btn1, btn2)
+        # number = message.contact.phone_number
+        # name1 = message.contact.first_name
+        # name2 = message.contact.last_name
+        final_message = "Отлично!"
+    bot.send_message(message.chat.id, final_message, parse_mode='html', reply_markup=markup)
