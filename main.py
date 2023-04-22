1369beecf2cc642635a7ca56ef42de929564413a
Yaroslav Samokhin <samokhin.yaroslav@gmail.com>
2023-04-22 22:34:41 +0300

    75%

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ number = []
 name1 = []
 name2 = []
 inf = []
+nik = []
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -19,6 +20,8 @@ def start(message):
     global name1
     global name2
     global inf
+    global nik
+    nik = []
     inf = []
     name1 = []
     name2 = []
@@ -37,6 +40,7 @@ def contact(message):
     global number
     global name1
     global name2
+    global nik
     if message.contact is not None:
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Я парень')
@@ -45,6 +49,7 @@ def contact(message):
         number = message.contact.phone_number
         name1 = message.contact.first_name
         name2 = message.contact.last_name
+        nik = message.from_user.username
         final_message = "Отлично!\nДавайте определимся с полом"
     bot.send_message(message.chat.id, final_message, parse_mode='html', reply_markup=markup)
 
@@ -58,30 +63,30 @@ def mess(message):
     global name1
     global name2
     global inf
+    global nik
     get_message_bot = message.text.strip().lower()
 
-    
     if get_message_bot == "я девушка":
-        inf.append(get_message_bot)
+        inf.append('Девушка')
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('S')
         btn2 = types.KeyboardButton('M')
         btn3 = types.KeyboardButton('L')
         btn4 = types.KeyboardButton('XL')
         markup.add(btn1, btn2, btn3, btn4)
-        file = open('Razmer.jpg', 'rb')
+        file = open('size.jpg', 'rb')
         bot.send_photo(message.chat.id, file)
         final_message = "Какой у вас размер?"
 
     if get_message_bot == "я парень":
-        inf.append(get_message_bot)
+        inf.append('Парень')
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('S')
         btn2 = types.KeyboardButton('M')
         btn3 = types.KeyboardButton('L')
         btn4 = types.KeyboardButton('XL')
         markup.add(btn1, btn2, btn3, btn4)
-        file = open('Razmer.jpg', 'rb')
+        file = open('size.jpg', 'rb')
         bot.send_photo(message.chat.id, file)
         final_message = "Какой у вас размер?"
 
@@ -147,6 +152,7 @@ def mess(message):
         final_message = "Хотите ли вы принт на свое футболке?"
 
     if get_message_bot == "🟡":
+        spis.append('yellow')
         inf.append(get_message_bot)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Да')
@@ -155,6 +161,7 @@ def mess(message):
         final_message = "Хотите ли вы принт на свое футболке?"
 
     if get_message_bot == "🟢":
+        spis.append('green')
         inf.append(get_message_bot)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Да')
@@ -163,6 +170,7 @@ def mess(message):
         final_message = "Хотите ли вы принт на свое футболке?"
 
     if get_message_bot == "🔵":
+        spis.append('blue')
         inf.append(get_message_bot)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Да')
@@ -171,6 +179,7 @@ def mess(message):
         final_message = "Хотите ли вы принт на свое футболке?"
 
     if get_message_bot == "⚫️":
+        spis.append('black')
         inf.append(get_message_bot)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Да')
@@ -179,6 +188,7 @@ def mess(message):
         final_message = "Хотите ли вы принт на свое футболке?"
 
     if get_message_bot == "⚪️":
+        spis.append('white')
         inf.append(get_message_bot)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Да')
@@ -187,6 +197,7 @@ def mess(message):
         final_message = "Хотите ли вы принт на свое футболке?"
 
     if get_message_bot == "🟣":
+        spis.append('purple')
         inf.append(get_message_bot)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Да')
@@ -199,12 +210,33 @@ def mess(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('К оформлению')
         markup.add(btn1)
-        final_message = "Окей. Ваша футболка выглядит так?"
+        final_message = "Окей. Ваша футболка выглядит так"
+        if 'red' in spis:
+            file = open('color/red.jpg', 'rb')
+            bot.send_photo(message.chat.id, file)
+        if 'blue' in spis:
+            file = open('color/blue.jpg', 'rb')
+            bot.send_photo(message.chat.id, file)
+        if 'black' in spis:
+            file = open('color/black.jpg', 'rb')
+            bot.send_photo(message.chat.id, file)
+        if 'white' in spis:
+            file = open('color/white.jpg', 'rb')
+            bot.send_photo(message.chat.id, file)
+        if 'yellow' in spis:
+            file = open('color/yellow.jpg', 'rb')
+            bot.send_photo(message.chat.id, file)
+        if 'purple' in spis:
+            file = open('color/purple.jpg', 'rb')
+            bot.send_photo(message.chat.id, file)
+        if 'green' in spis:
+            file = open('color/green.jpg', 'rb')
+            bot.send_photo(message.chat.id, file)
 
     if get_message_bot == "да":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Аниме')
-        btn2 = types.KeyboardButton('Игры')
+        btn2 = types.KeyboardButton('Корейцы')
         btn3 = types.KeyboardButton('К пацанами')
         markup.add(btn1, btn2, btn3)
         final_message = "В каком стиле должен быть ваш принт?"
@@ -215,82 +247,82 @@ def mess(message):
         btn1 = types.KeyboardButton('Заказать')
         markup.add(btn1)
         final_message = "Хорошо. Ваша футболка будет выглядеть вот так"
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'blue' in spis:
+            file = open('anime/blue_anime.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
         if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+            file = open('anime/red_anime.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'green' in spis:
+            file = open('anime/green_anime.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'black' in spis:
+            file = open('anime/black_anime.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'purple' in spis:
+            file = open('anime/purple_anime.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'yellow' in spis:
+            file = open('anime/yellow_anime.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'white' in spis:
+            file = open('anime/white_anime.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
 
-    if get_message_bot == "buhs":
-        inf.append('Принт с Аниме')
+    if get_message_bot == "корейцы":
+        inf.append('Принт с Корейцами')
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Заказать')
         markup.add(btn1)
         final_message = "Хорошо. Ваша футболка будет выглядеть вот так"
         if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+            file = open('korean/red2.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'blue' in spis:
+            file = open('korean/blue2.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'green' in spis:
+            file = open('korean/green2.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'white' in spis:
+            file = open('korean/white2.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'yellow' in spis:
+            file = open('korean/yellow2.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'black' in spis:
+            file = open('korean/black2.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'purple' in spis:
+            file = open('korean/purple2.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
 
     if get_message_bot == "к пацанам":
-        inf.append('Принт с пацанами')
+        inf.append('Принт с мемами')
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('Заказать')
         markup.add(btn1)
         final_message = "Хорошо. Ваша футболка будет выглядеть вот так"
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'blue' in spis:
+            file = open('drill/blue3.jpeg', 'rb')
             bot.send_photo(message.chat.id, file)
         if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+            file = open('drill/red3.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'green' in spis:
+            file = open('drill/green3.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'black' in spis:
+            file = open('drill/black3.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'purple' in spis:
+            file = open('drill/purple3.jpeg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'yellow' in spis:
+            file = open('drill/yellow3.jpg', 'rb')
             bot.send_photo(message.chat.id, file)
-        if 'red' in spis:
-            file = open('odnot/red.jpg', 'rb')
+        if 'white' in spis:
+            file = open('drill/white3.jpeg', 'rb')
             bot.send_photo(message.chat.id, file)
 
 
@@ -299,5 +331,22 @@ def mess(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         btn1 = types.KeyboardButton('К оформлению')
         markup.add(btn1)
-        bot.send_message(id, f"Заявка {chet}\nПользователь:\n{name1, name2}\nНомер:\n{number}\n{inf[0]}\n{inf[1]}\n{inf[2]}")
-        final_message = "Окей. Ваша футболка выглядит так?"
+        bot.send_message(578898951, f"Заявка {chet}\nПользователь:\n{name1, name2}\nНомер:\n+{number}\n@{nik}\n\n{inf[0]}\nРазмер:{inf[1]}\nцвет - {inf[2]}\n{inf[3]}")
+        print(inf)
+        final_message = "три два раз"
+
+    if get_message_bot == "заказать":
+        chet += 1
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+        btn1 = types.KeyboardButton('К оформлению')
+        markup.add(btn1)
+        bot.send_message(578898951, f"Заявка {chet}\nПользователь:\n{name1, name2}\nНомер:\n+{number}\n@{nik}\n\n{inf[0]}\nРазмер:{inf[1]}\nцвет - {inf[2]}\n{inf[3]}")
+        print(inf)
+        final_message = "раз два три"
+
+        #ВОТ СЮДА КИДАЙ СПИСОК СЛОВ
+
+    bot.send_message(message.chat.id, final_message, parse_mode='html', reply_markup=markup)
+
+
+bot.polling(none_stop=True)
